c92cce2296b7241a4eefa0bc64e98c8001d0809f
harika <harikasree.t@comakeit.com>
2023-04-28 23:48:33 +0530

    docs redirection

--- src/pages/Docs/DocHome.js
+++ src/pages/Docs/DocHome.js
@@ -1,22 +1,15 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
 import { Heading, Container } from "@chakra-ui/react";
 
 export default function DocHome() {
   useEffect(() => {
     const timeout = setTimeout(() => {
-     
       window.open(process.env.REACT_APP_DOCS_URL);
+      window.location.replace("./");
     }, 10);
 
     return () => clearTimeout(timeout);
   }, []);
 
-    return (
-      <Container maxW="2xl" marginTop="16px">
-        <Heading size="xl" fontWeight="extrabold" mb={4} marginBottom="50px">
-        Redirecting...
-        </Heading>
-      </Container>
-    );
-  }
-  
+  return <></>
+}
