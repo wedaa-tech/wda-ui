bd1226f6adb662d002ae9b15ba27753452d6b1b2
dheeraj-gantala <saidheeraj.gantala@gmail.com>
2023-11-13 22:47:00 +0530

    Added Options in NavBar

--- src/index.js
+++ src/index.js
@@ -4,15 +4,11 @@ import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
 import { extendTheme } from '@chakra-ui/react';
 import { buttonTheme } from './styles/Button';
-import "@fontsource/poppins"; 
+import '@fontsource/poppins';
 
 export const theme = extendTheme({
     components: {
         Button: buttonTheme,
-        fonts: {
-            heading: `'Poppins', sans-serif`,
-            body: `'Poppins', sans-serif`,
-        },
     },
 });
 
