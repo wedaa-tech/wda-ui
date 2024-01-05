eb1aee83ab056409939133b08df2f09f6ee2765c
raxkumar-wedaa <rakesh.c@wedaa.tech>
2024-01-05 15:39:43 +0530

    - modified the check for component description display

--- src/pages/Rewiew/Readme.js
+++ src/pages/Rewiew/Readme.js
@@ -5,31 +5,30 @@ import {
     DOCUSAURUS_README,
     ANGULAR_README,
     GOMICRO_README,
-    JAVA_README,
+    SPRING_README,
     GATEWAY_README,
     GLOBAL_README,
 } from './ReadmeOutputs';
 
 const Readme = ({ nodeType, nodeData }) => {
     const [readmeData, setReadmeData] = useState('');
+    
+    const readmeMappings = {
+        'Service:spring': SPRING_README,
+        'Gateway:java': GATEWAY_README,
+        'UI:react': REACT_README,
+        'UI:docusaurus': DOCUSAURUS_README,
+        'UI:angular': ANGULAR_README,
+        'Service:gomicro': GOMICRO_README,
+        'default': GLOBAL_README
+    };
+    
     useEffect(() => {
-        if (nodeType && nodeType.includes('Service') && nodeData.applicationFramework.includes('spring')) {
-            setReadmeData(JAVA_README(nodeData));
-        } else if (nodeType && nodeType.includes('Gateway')) {
-            setReadmeData(GATEWAY_README(nodeData));
-        } else if (nodeType && nodeType.includes('UI') && nodeData.applicationFramework.includes('react')) {
-            setReadmeData(REACT_README(nodeData));
-        } else if (nodeType && nodeType.includes('UI') && nodeData.applicationFramework.includes('docusaurus')) {
-            setReadmeData(DOCUSAURUS_README(nodeData));
-        } else if (nodeType && nodeType.includes('UI') && nodeData.applicationFramework.includes('angular')) {
-            setReadmeData(ANGULAR_README(nodeData));
-        } else if (nodeType && nodeType.includes('Service') && nodeData.applicationFramework.includes('gomicro')) {
-            setReadmeData(GOMICRO_README(nodeData));
-        } else {
-            setReadmeData(GLOBAL_README());
-        }
-    }, [nodeType]);
-
+        const key = `${nodeType}:${nodeData.applicationFramework}`;
+        const readmeFunction = readmeMappings[key] || readmeMappings['default'];
+        setReadmeData(readmeFunction(nodeData));
+    }, [nodeType, nodeData.applicationFramework]);
+    
     return <MarkdownPreview wrapperElement={{ 'data-color-mode': 'light' }} source={readmeData} />;
 };
 
