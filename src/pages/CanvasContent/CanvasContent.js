f25426e5dd0162cdc68b126b5d60587b185b2ebf
raxkumar-wedaa <rakesh.c@wedaa.tech>
2024-03-28 11:35:19 +0530

    changing css classNames

--- src/pages/CanvasContent/CanvasContent.js
+++ src/pages/CanvasContent/CanvasContent.js
@@ -7,14 +7,14 @@ import './CanvasContent.css';
 function CanvasContent() {
     return (
         <Box flex={'1'} display={'flex'} justifyContent={'center'} alignItems={'center'} transition={'all 3s ease-in-out'}>
-            <Box className="boxStyle">
+            <Box className="box-style">
                 <div className="content">
-                    <FiUploadCloud className="cloudIcon" />
+                    <FiUploadCloud className="cloud-icon" />
                 </div>
                 <Text className="text">Design your application architecture here</Text>
-                <Text className="subText">Click next to auto generate code and setup infrastructure</Text>
+                <Text className="sub-text">Click next to auto generate code and setup infrastructure</Text>
                 <Button>
-                    Drag & Drop <ArrowRightIcon className="arrowIcon" />
+                    Drag & Drop <ArrowRightIcon className="arrow-icon" />
                 </Button>
             </Box>
         </Box>
