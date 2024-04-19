7a4990e96cf8b6b4d260a337d8cdd954c6035ed1
harsha-wedaa <sriharsha.kvsssrh@wedaa.tech>
2024-04-19 15:57:10 +0530

    - Download zip added on  prototype page - refarchitecture flow changed - added generating state for prototype - preview page added generate for generation

--- src/pages/Review/Deployment.js
+++ src/pages/Review/Deployment.js
@@ -1,28 +1,55 @@
 import React, { useEffect, useState } from 'react';
 import { useReactFlow } from 'reactflow';
-import { Table, Thead, Tbody, Tr, Th, Td, Box } from '@chakra-ui/react';
+import { Table, Thead, Tbody, Tr, Th, Td, Box, Button } from '@chakra-ui/react';
 
-const Deployment = ({ deploymentData }) => {
+const Deployment = ({ deploymentData, onSubmit, generateZip, parentId }) => {
+    let generateCodeButton = (
+        <Button
+            mx={4}
+            my={2}
+            colorScheme="blue"
+            onClick={() => {
+                onSubmit(deploymentData, true);
+            }}
+            hidden={parentId=='admin'}
+            minH={'48px'}
+            flexGrow={1} 
+            style={{ position: 'absolute', bottom: "12px",width:'85%' }} // Set position to bottom
+        >
+            Generate Code
+        </Button>
+    );
+
     if (!deploymentData?.deployment) {
-        return <Box p={4}>Deployment Data Not Found.</Box>;
+        return (
+            <>
+                <Box p={4}>Deployment Data Not Found.</Box>
+                {generateCodeButton}
+            </>
+        );
     } else {
         return (
-            <Table variant="simple">
-                <Thead>
-                    <Tr>
-                        <Th>Property</Th>
-                        <Th>Value</Th>
-                    </Tr>
-                </Thead>
-                <Tbody>
-                    {Object.keys(deploymentData.deployment).map(key => (
-                        <Tr key={key}>
-                            <Td>{key}</Td>
-                            <Td>{deploymentData.deployment[key]}</Td>
-                        </Tr>
-                    ))}
-                </Tbody>
-            </Table>
+            <>
+                <Box display="flex" flexDirection="column" position="relative">
+                    <Table variant="simple">
+                        <Thead>
+                            <Tr>
+                                <Th>Property</Th>
+                                <Th>Value</Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            {Object.keys(deploymentData.deployment).map(key => (
+                                <Tr key={key}>
+                                    <Td>{key}</Td>
+                                    <Td>{deploymentData.deployment[key]}</Td>
+                                </Tr>
+                            ))}
+                        </Tbody>
+                    </Table>
+                </Box>
+                {generateCodeButton}
+            </>
         );
     }
 };
