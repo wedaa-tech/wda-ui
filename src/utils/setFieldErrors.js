bd2b0d657507c6bd9753c06b6124c2f9ea618ce0
raxkumar-wedaa <rakesh.c@wedaa.tech>
2024-04-22 12:50:38 +0530

    fileds and errors

---
+++ src/utils/setFieldErrors.js
@@ -0,0 +1,27 @@
+const setFieldErrors = (fields, errors) => {
+    fields.forEach(field => {
+        switch (field.key) {
+            case 'applicationName':
+                field.error = errors.duplicateApplicationNameError
+                    ? 'Application name already exists. Please choose a unique name.'
+                    : errors.appNameCheck
+                    ? 'Application Name should not contain -, _ or numbers.'
+                    : '';
+                break;
+            case 'theme':
+                field.error = errors.themeError ? 'Select a Theme.' : '';
+                break;
+            case 'serverPort':
+                field.error = errors.portValidationError ? errors.portValidationError.message : '';
+                break;
+            case 'packageName': 
+                field.error = errors.packageNameCheck ? 'Enter a valid package name' : '';
+                break;
+            default:
+                field.error = '';
+                break;
+        }
+    });
+};
+
+export default setFieldErrors;
